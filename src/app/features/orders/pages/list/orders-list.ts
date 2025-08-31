50c8d7961ff3e7bdb544d74389a93409c83e5d2d
ameenelnaggar <ameenelnaggar9@gmail.com>
2025-08-31 12:59:28 +0300

    Edit Files

---
+++ src/app/features/orders/pages/list/orders-list.ts
@@ -0,0 +1,102 @@
+import {
+  Component,
+  ViewChild,
+  TemplateRef,
+  inject,
+  viewChild,
+  output,
+} from '@angular/core';
+import { Table, TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { IColumnDefinition } from '../../../../shared/interfaces/column-definition.model';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { TableHeader } from '../../../../shared/components/table-header/table-header';
+import { TableStore } from '../../../../store/table.store';
+
+@Component({
+  selector: 'app-orders-list',
+  standalone: true,
+  imports: [
+    TableModule,
+    DialogModule,
+    ConfirmDialogModule,
+    ToastModule,
+    ButtonModule,
+    InputTextModule,
+    FormsModule,
+    CommonModule,
+    IconFieldModule,
+    InputIconModule,
+    DynamicDialogModule,
+    TableHeader,
+  ],
+  templateUrl: './orders-list.html',
+  styleUrl: './orders-list.css',
+})
+export class OrdersList {
+  private tableStore = inject(TableStore);
+
+  table = viewChild.required<Table>('dt');
+
+  records = this.tableStore.records;
+  columns = this.tableStore.columns;
+  loading = this.tableStore.isLoading;
+  error = this.tableStore.error;
+
+  ngOnInit() {
+    this.tableStore.setContext('orders');
+    this.tableStore.setEndpoints({
+      data: 'orders-data',
+      metadata: 'orders-metadata',
+    });
+    this.tableStore.loadData();
+    this.tableStore.loadMetadata();
+  }
+
+  confirmDelete(code: string) {
+    this.tableStore.deleteRecord(code);
+  }
+
+  onRowSelectEvent(event: any) {
+    this.onShowUpdateDialog(event.data);
+  }
+
+  onFilterGlobal(value: string) {
+    this.table().filterGlobal(value, 'contains');
+  }
+
+  onShowCreateDialog() {
+    const ref = this.tableStore.showCreateDialog();
+    ref.onClose.subscribe((result) => {
+      if (result) {
+        this.tableStore.create(result);
+      }
+    });
+  }
+
+  onShowUpdateDialog(record: any) {
+    const ref = this.tableStore.showUpdateDialog(record);
+    ref.onClose.subscribe((result) => {
+      if (result) {
+        this.tableStore.update(result);
+      }
+    });
+  }
+
+  onShowViewDialog(record: any) {
+    const ref = this.tableStore.showViewDialog(record);
+    ref.onClose.subscribe((result) => {
+      if (result) {
+        console.log('View Dialog closed with result:', result);
+      }
+    });
+  }
+}
---
+++ src/app/features/products/pages/list/products-list.ts
@@ -0,0 +1,102 @@
+import {
+  Component,
+  ViewChild,
+  TemplateRef,
+  inject,
+  viewChild,
+  output,
+} from '@angular/core';
+import { Table, TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { IColumnDefinition } from '../../../../shared/interfaces/column-definition.model';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { TableHeader } from '../../../../shared/components/table-header/table-header';
+import { TableStore } from '../../../../store/table.store';
+
+@Component({
+  selector: 'app-products-list',
+  standalone: true,
+  imports: [
+    TableModule,
+    DialogModule,
+    ConfirmDialogModule,
+    ToastModule,
+    ButtonModule,
+    InputTextModule,
+    FormsModule,
+    CommonModule,
+    IconFieldModule,
+    InputIconModule,
+    DynamicDialogModule,
+    TableHeader,
+  ],
+  templateUrl: './products-list.html',
+  styleUrl: './products-list.css',
+})
+export class ProductsList {
+  private tableStore = inject(TableStore);
+
+  table = viewChild.required<Table>('dt');
+
+  records = this.tableStore.records;
+  columns = this.tableStore.columns;
+  loading = this.tableStore.isLoading;
+  error = this.tableStore.error;
+
+  ngOnInit() {
+    this.tableStore.setContext('products');
+    this.tableStore.setEndpoints({
+      data: 'products-data',
+      metadata: 'products-metadata',
+    });
+    this.tableStore.loadData();
+    this.tableStore.loadMetadata();
+  }
+
+  confirmDelete(code: string) {
+    this.tableStore.deleteRecord(code);
+  }
+
+  onRowSelectEvent(event: any) {
+    this.onShowUpdateDialog(event.data);
+  }
+
+  onFilterGlobal(value: string) {
+    this.table().filterGlobal(value, 'contains');
+  }
+
+  onShowCreateDialog() {
+    const ref = this.tableStore.showCreateDialog();
+    ref.onClose.subscribe((result) => {
+      if (result) {
+        this.tableStore.create(result);
+      }
+    });
+  }
+
+  onShowUpdateDialog(record: any) {
+    const ref = this.tableStore.showUpdateDialog(record);
+    ref.onClose.subscribe((result) => {
+      if (result) {
+        this.tableStore.update(result);
+      }
+    });
+  }
+
+  onShowViewDialog(record: any) {
+    const ref = this.tableStore.showViewDialog(record);
+    ref.onClose.subscribe((result) => {
+      if (result) {
+        console.log('View Dialog closed with result:', result);
+      }
+    });
+  }
+}
